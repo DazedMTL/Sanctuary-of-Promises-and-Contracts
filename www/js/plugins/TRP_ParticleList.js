8025e4c6b3a3bbea305fc53846bb199b26cf3301
Dazed <dazedanon@gmail.com>
2024-03-14 21:43:17 -0500

    Initial Commit

---
+++ www/js/plugins/TRP_ParticleList.js
@@ -0,0 +1,12 @@
+/*:
+ * @help
+ * fog_w2 <対象:screen> ＠2021/6/12
+ *
+ * @requiredAssets img/particles/cloud3
+ * @requiredAssets img/particles/cloud2
+ * @requiredAssets img/particles/cloud1
+ * @requiredAssets img/particles/_ANIM_60FPS_AC3T001_Cast01a_23
+ */
+PluginManager._parameters.trp_particlelist = {
+	animImages:["ANIM:60FPS_AC3T001_Cast01a:23"]
+};
